652a26c10a6a51c320c2b6e8c7468bb99c9f7a33
mbaddar <mbaddar2@gmail.com>
2022-05-19 08:48:57 +0200

    time limit exceeded

--- continous_subarray_sum.py
+++ continous_subarray_sum.py
@@ -14,9 +14,12 @@ Line 40 in _driver (Solution.py)
 Line 51 in <module> (Solution.py)
 test case : https://leetcode.com/submissions/detail/701961972/testcase/ 
 """
+
+
 class Solution:
-    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+    def checkSubarraySum_attempt1(self, nums: List[int], k: int) -> bool:
         n = len(nums)
+        # TODO : make memory complexity o(1) or o(n)
         dp = np.zeros(shape=(n, n))
         for i in range(n):
             dp[i][i] = nums[i]
@@ -25,6 +28,34 @@ class Solution:
                 dp[i][j] = dp[i][j - 1] + nums[j]
                 if dp[i][j] % k == 0:
                     print(f"Found subarray : {nums[i:j]}")
+                    return True
+        return False
+
+    def checkSubarraySum(self, nums: List[int], k: int):
+        """
+        https://leetcode.com/submissions/detail/702604577/testcase/
+        # TODO Time limit exceeded https://leetcode.com/problems/continuous-subarray-sum/submissions/
+        :param nums:
+        :param k:
+        :return:
+        """
+        n = len(nums)
+        arr_0_k = np.zeros(n)
+        arr_0_k[0] = nums[0]
+        for i in range(1, n):
+            arr_0_k[i] = arr_0_k[i - 1] + nums[i]
+        arr_k_n_1 = np.zeros(n)
+        arr_k_n_1[n - 1] = nums[n - 1]
+        for j in range(n - 2, -1, -1):
+            arr_k_n_1[j] = arr_k_n_1[j + 1] + nums[j]
+        tot_sum = arr_0_k[n - 1]  # avoid recalculation of sums
+        for i in range(n):
+            for j in range(i + 1, n):
+                val_low = 0 if i == 0 else arr_0_k[i - 1]
+                val_high = 0 if j == n - 1 else arr_k_n_1[j + 1]
+                subSum = tot_sum - val_high - val_low
+                if subSum % k == 0:
+                    print(nums[i:j])
                     return True
         return False
 

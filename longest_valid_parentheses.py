85b67c25fb5a5299ba261bf20e504a535a1752e1
mbaddar <mbaddar2@gmail.com>
2022-08-02 15:19:18 +0200

    accepted

--- longest_valid_parentheses.py
+++ longest_valid_parentheses.py
@@ -1,7 +1,7 @@
 # https://leetcode.com/problems/longest-valid-parentheses/
+# Accepted https://leetcode.com/problems/longest-valid-parentheses/submissions/
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
-        max_interval = 0
         intervals = []
         stk = []
         for i, e in enumerate(s):
@@ -25,7 +25,7 @@ class Solution:
 
                         intervals.append(interval)
 
-        print(intervals)
+        # print(intervals)
         merged_intervals = []
 
         for j in range(len(intervals)):
@@ -39,7 +39,7 @@ class Solution:
                     merged_intervals.append(intervals[j])
             else:
                 merged_intervals.append(intervals[j])
-        print(merged_intervals)
+        # print(merged_intervals)
         max_interval = 0
         for k in range(len(merged_intervals)):
             max_interval = max([max_interval, merged_intervals[k][1] - merged_intervals[k][0] + 1])
@@ -49,7 +49,7 @@ class Solution:
 if __name__ == '__main__':
     q = Solution()
     s = "()((())())"
-    #s = "()(()"
+    # s = "()(()"
     s = "(()())"
     r = q.longestValidParentheses(s)
     print(r)

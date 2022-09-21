3029cc3ff6403c12d45254942c2085251babfb0b
mbaddar <mbaddar2@gmail.com>
2022-09-21 09:27:25 +0200

    submitted

--- remove_duplicates_from_sorted_array.py
+++ remove_duplicates_from_sorted_array.py
@@ -1,5 +1,25 @@
 # https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/
+# submission
+# https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/submissions/
+
+import random
 from typing import List
+
+import numpy as np
+from big_o import big_o, datagen
+
+from my_big_o import MyBigO
+
+
+def mydatagen(n):
+    i = 0
+    l = []
+    while i < n:
+        repeat = random.randint(1, 4)
+        block = np.repeat(a=(i + 1), repeats=repeat)
+        i += repeat
+        l.extend(block)
+    return np.array(l)
 
 
 class Solution:
@@ -7,7 +27,8 @@ class Solution:
     MIN_NUMBER = int(-1e4)
     DUMMY_NUM = MIN_NUMBER - 1
 
-    def removeDuplicates(self, nums: List[int]) -> int:
+    @staticmethod
+    def removeDuplicates(nums: List[int]) -> int:
         n = len(nums)
         # pass 1 , put displacements
         block_counter = 1
@@ -21,6 +42,7 @@ class Solution:
                     coded_displacement = Solution.MAX_NUMBER + cum_block_diff
                     nums[i - 1] = coded_displacement
                 block_counter = 1
+        cum_block_diff += max(block_counter - 2, 0)
         # pass 2 - displacement
         for j in range(n):
             if nums[j] > Solution.MAX_NUMBER:
@@ -42,14 +64,24 @@ class Solution:
 if __name__ == '__main__':
     sol = Solution()
 
-    nums = [0, 0, 1, 1, 1, 1, 2, 3, 3]
-    r = sol.removeDuplicates(nums)
-    print(nums[:r])
+    # nums = [0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3]
+    # r = sol.removeDuplicates(nums)
+    # print(nums[:r])
 
-    nums = [1, 1, 1, 2, 2, 3]
-    r = sol.removeDuplicates(nums)
-    print(nums[:r])
+    # nums = [1, 1, 1, 2, 2, 3, 3, 3, 3, 4, 4, 4]
+    # r = sol.removeDuplicates(nums)
+    # print(nums[:r])
 
-    nums = [1, 1, 1, 2, 2, 3, 3, 3, 4]
+    # nums = [1, 1, 1, 2, 2, 3, 3, 3, 4]
+    # r = sol.removeDuplicates(nums)
+    # print(nums[:r])
+
+    nums = [1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 4, 4, 4, 4, 4, 4, ]
     r = sol.removeDuplicates(nums)
     print(nums[:r])
+    # positive_integers_gen = lambda n: datagen.integers(n=n, min_=0, max_=10000)
+    # best, others = big_o(func=Solution.removeDuplicates, data_generator=positive_integers_gen, min_n=10000,
+    #                      max_n=100000)
+    # print(best)
+
+    # MyBigO.get_time_complexity(func=Solution.removeDuplicates,data_gen=mydatagen)
